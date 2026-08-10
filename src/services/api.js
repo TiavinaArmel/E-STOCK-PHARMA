d7146175bf31d8996fc5a7f1b73bd57cc65d2c78
TiavinaArmel <ajrnt728@gmail.com>
2026-08-10 22:30:53 +0300

    correction base URL de api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-
+// On utilise bien la même variable partout !
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://tiavinaarmel.alwaysdata.net/pharmacie';
 
-
-
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: API_BASE_URL, // <--- Correction ici : API_BASE_URL au lieu de API_URL
     headers: {
         "Content-Type": "application/json"
     }
 });
+
 api.defaults.withCredentials = true;
 
 export default api;
